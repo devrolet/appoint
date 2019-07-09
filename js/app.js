30a7e1656eba7ee875aaa79ee1807df1bd4260a6
Chet Hill <chetdhill@gmail.com>
2019-07-09 10:52:41 -0400

    Declare Form Variables

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let DB;
+
+const form = document.querySelector('form'),
+             artistName = document.querySelector('#artist-name'),
+             labelName = document.querySelector('#label-name'),
+             phone = document.querySelector('#phone'),
+             date = document.querySelector('#date'),
+             hour = document.querySelector('#hour'),
+             work = document.querySelector('#work'),
+             appointments = document.querySelector('#appointments'),
+             appointmentTitle = document.querySelector('#appointment-title');
