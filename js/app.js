8f8b390e15f9854e6b15e9f18420e2fcc9ad7dcf
Chet Hill <chetdhill@gmail.com>
2019-07-09 11:10:54 -0400

    Create Schema

--- js/app.js
+++ js/app.js
@@ -20,9 +20,23 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     // if successful, assign the result to the instance
     AppointmentDB.onsuccess = function() {
-        console.log('Database Ready');
+        // console.log('Database Ready');
 
         // save the result
         DB = AppointmentDB.result;
+    }
+
+    // This message runs once (creates schema)
+    AppointmentDB.onupgradeneeded = function(e) {
+        // the event will be the database
+        let db = e.target.result;
+
+        // create an object store
+        // keypath is the the indexes for database
+        let objectStore = db.createObjectStore('apppointments', {keypath: 'key', autoIncrement: true} );
+        // createIndex: 1. field name 2. keypath 3. options
+        objectStore.createIndex('artistname', 'artistname', {unique: false} );
+
+        console.log('Database ready and fields created');
     }
 });
