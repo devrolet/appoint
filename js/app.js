468a020219c246e5474f74c078f5c876f6f8133c
Chet Hill <chetdhill@gmail.com>
2019-07-09 10:59:37 -0400

    Establish IndexedDB Database

--- js/app.js
+++ js/app.js
@@ -9,3 +9,20 @@ const form = document.querySelector('form'),
              work = document.querySelector('#work'),
              appointments = document.querySelector('#appointments'),
              appointmentTitle = document.querySelector('#appointment-title');
+
+document.addEventListener('DOMContentLoaded', () => {
+    // create the database
+    let AppointmentDB = window.indexedDB.open('appointment', 1);
+
+    // if there is an error
+    AppointmentDB.onerror = function() {
+        console.log('There was an error');
+    }
+    // if successful, assign the result to the instance
+    AppointmentDB.onsuccess = function() {
+        console.log('Database Ready');
+
+        // save the result
+        DB = AppointmentDB.result;
+    }
+});
